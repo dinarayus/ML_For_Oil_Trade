e0938b269da65f50e262d8815fea8e74dc847b83
Dinara <dinarakhubiyeva@gmail.com>
2022-06-02 23:20:54 +0700

    commit changes

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ plt.title(" Oil Price Trend")
 print('Hello!')
 print('My project')
 #Изображаем всю базу данных в виде графика
+
+a = int(input())
+if a > 2:
+    pass
+else:
+    if a == 1:
+        pass
+    else:
+        if a == 0:
+            pass
+        else:
+            if a == -1:
+                pass
+            else:pass
+
+if a > 2:
+    pass
+elif a == 0:
+    pass
+elif a == -1:
+    pass
+else: pass
